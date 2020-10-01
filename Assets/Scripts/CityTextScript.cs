5b458513b23cc76fed750d0b68a97b91fe11961c
parthshukla <parthshuklaa@gmail.com>
2020-10-02 00:33:28 +0530

    Update CityTextScript.cs
    Added relevant information necessary

--- Assets/Scripts/CityTextScript.cs
+++ Assets/Scripts/CityTextScript.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+//this cript utilises textMeshPro library of Unity3D to interact with the applicaiton and publish user location using weather.io API
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
